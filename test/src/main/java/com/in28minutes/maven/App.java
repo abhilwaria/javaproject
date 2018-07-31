5ce133ae89f02e7092144fabd8ffbe195776f262
abhilwaria <ankur.bhilwaria@gmail.com>
2018-07-31 14:32:10 +0530

    Update App.java
    updated

--- test/src/main/java/com/in28minutes/maven/App.java
+++ test/src/main/java/com/in28minutes/maven/App.java
@@ -6,7 +6,7 @@ package com.in28minutes.maven;
  */
 public class App 
 {
-    public static void main( String[] args )
+    public static void main( String[] args );
 
 
 	public int calculateSomething() {
